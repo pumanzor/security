8448f086dc74cc24540677fefea7da14590db849
Pablo Umanzor A <info@redlibre.cl>
2016-10-26 03:14:35 -0300

    Update rs485_smodbus_slave.ino

--- protocol/modbus/lab/rs485_smodbus_slave.ino
+++ protocol/modbus/lab/rs485_smodbus_slave.ino
@@ -5,8 +5,8 @@
 //////////////// registers of your slave ///////////////////
 enum 
 {
-  ADC_VAL,     
-  PWM_VAL,        
+  reg0_VAL,     
+  reg1_VAL,        
   HOLDING_REGS_SIZE
 };
 
@@ -16,7 +16,7 @@ unsigned int holdingRegs[HOLDING_REGS_SIZE]; // function 3 and 16 register array
 void setup()
 {
   modbus_configure(&Serial, 19200, SERIAL_8N1, 1, 2, HOLDING_REGS_SIZE, holdingRegs);
-  modbus_update_comms(19200, SERIAL_8N1, 229); //id del esclavo
+  modbus_update_comms(19200, SERIAL_8N1, 229);
   
   pinMode(LED, OUTPUT);
 }
@@ -28,8 +28,8 @@ void loop()
  // holdingRegs[ADC_VAL] = analogRead(A0);
   
  digitalWrite(LED, HIGH);
- delay(holdingRegs[PWM_VAL]);
+ delay(holdingRegs[reg0_VAL]);
  digitalWrite(LED, LOW);
- delay(holdingRegs[PWM_VAL]);
+ delay(holdingRegs[reg1_VAL]);
  
 }
