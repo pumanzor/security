54b1b05d273c7a5095bed1bc9e3e5e90e3047992
Pablo Umanzor A <info@redlibre.cl>
2016-10-25 21:53:45 -0300

    Update rs485_smodbus_master.ino

--- protocol/modbus/lab/rs485_smodbus_master.ino
+++ protocol/modbus/lab/rs485_smodbus_master.ino
@@ -4,10 +4,10 @@
 #define timeout 1000
 #define polling 2000
 #define retry_count 10
-
 #define TxEnablePin 2 
-
 #define LED 9
+//ID slave node 
+#define ID 10
 
 #define TOTAL_NO_OF_REGISTERS 1
 
@@ -17,8 +17,6 @@ enum
   PACKET2,
   TOTAL_NO_OF_PACKETS // leave this last entry
 };
-int sensorValue = 0;        // value read from the pot
-int outputValue = 0; 
 
 Packet packets[TOTAL_NO_OF_PACKETS];
 
@@ -27,8 +25,7 @@ unsigned int regs[TOTAL_NO_OF_REGISTERS];
 void setup()
 {
   modbus_construct(&packets[PACKET1], 1, READ_HOLDING_REGISTERS, 0, 1, 0);
-  modbus_construct(&packets[PACKET2], 229, PRESET_MULTIPLE_REGISTERS, 1, 1, 0);
-  //229 es el id del esclavo
+  modbus_construct(&packets[PACKET2], ID, PRESET_MULTIPLE_REGISTERS, 1, 1, 0);
   modbus_configure(&Serial, baud, SERIAL_8N1, timeout, polling, retry_count, TxEnablePin, packets, TOTAL_NO_OF_PACKETS, regs);
   
   pinMode(LED, OUTPUT);
